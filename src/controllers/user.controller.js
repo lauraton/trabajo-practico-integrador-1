134499b8b883b6cb793eff18913a6517f2be85f2
lauraton <nickk5926@gmail.com>
2026-09-07 11:52:37 -0300

    Feat & WIP: Se crearon las validaciones para crear un usuario, el controlador para llevarlo a cabo y se agregó una validación global en validator.js

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,13 @@
+import { matchedData, validationResult } from "express-validator";
+import { User } from "../models/user.model.";
+
+export const createUser = async (req, res) => {
+    try {
+        const validatedData = matchedData(req);
+        const user = await User.create(validatedData)
+        return res.status(201).json({message: "Usuario creado correctamente", user})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Error interno del servidor"});
+    }
+}
---
+++ src/middlewares/validations/user.validation.js
@@ -0,0 +1,59 @@
+import { body, param } from "express-validator";
+import { User } from "../../models/user.model.js";
+
+export const createUserValidation = [
+    body("username")
+        .notEmpty()
+        .withMessage("Username no puede estar vacío")
+        .isString()
+        .withMessage("El username debe ser un string")
+        .isLength( { min: 3, max: 20 })
+        .withMessage("Username debe contener al menos 3 caracteres, con un máximo de 20")
+        .custom(async (username) => {
+            const existingUsername = await User.findOne({
+                where: {username}
+            });
+            if (existingUsername) {
+                throw new Error("El username ya está en uso.")
+            }
+            return true;
+        }),
+    body("email")
+        .notEmpty()
+        .withMessage("Email no puede estar vacío")
+        .isEmail()
+        .withMessage("Email debe tener un formato válido")
+        .isLength({ max: 100 })
+        .withMessage("Email no debe superar los 100 caracteres")
+        .custom(async (email) => {
+            const existingEmail = User.findOne({ where: {email}});
+            if (existingEmail) {
+                throw new Error("Ese email ya está registrado")
+            }
+            return true;
+        }),
+    body("password")
+        .notEmpty()
+        .withMessage("Password no puede estar vacío")
+        .isString()
+        .withMessage("Password debe ser un string"),
+    body("role")
+        .notEmpty()
+        .withMessage("role no puede estar vacío"),
+
+];
+
+export const userIdValidation = [
+    param("id")
+        .isInt({ min: 1 })
+        .withMessage("El ID debe ser un entero positivo")
+        .custom(async (id) => {
+            const user = await User.findByPk(id);
+
+            if (!user) {
+                throw new Error("El usuario no existe");
+            }
+
+            return true;
+        })
+];
